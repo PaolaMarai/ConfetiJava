149118f2f62fb96e438185ee324004cbc4f44a70
jeth18 <36642059+jeth18@users.noreply.github.com>
2019-05-20 15:44:43 -0500

    Se arreeglo el server

--- ServerConfetti/src/serverconfetti/ServerConfetti.java
+++ ServerConfetti/src/serverconfetti/ServerConfetti.java
@@ -1,4 +1,4 @@
- /*
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -8,36 +8,38 @@ package serverconfetti;
 import interfacesconfetti.ICliente;
 import interfacesconfetti.IServer;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.rmi.AlreadyBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-public class ServerConfetti implements IServer {
-    private final int PORT = 3333;
-    private List<ICliente> clientes = new ArrayList<>();
-  
-     private void init() throws RemoteException {
-    try {
-      String direccion = (InetAddress.getLocalHost()).toString();
-      int puerto = 3333;
-      System.out.println("Iniciando servidor en " + direccion + ":" + puerto);
+public class ServerConfetti extends UnicastRemoteObject implements IServer {
 
-      Registry registro = LocateRegistry.createRegistry(puerto);
-      registro.bind("Servidor", (IServer) this);
+    private final List<ICliente> clientes;
 
-    } catch (UnknownHostException | AlreadyBoundException | RemoteException ex) {
-      Logger.getLogger(ServerConfetti.class.getName()).log(Level.SEVERE, null, ex);
+    private void init() throws RemoteException {
+        try {
+            String direccion = (InetAddress.getLocalHost()).toString();
+            int puerto = 3333;
+            System.out.println("Iniciando servidor en " + direccion + ":" + puerto);
+
+            Registry registro = LocateRegistry.createRegistry(puerto);
+            registro.bind("ConfettiServer", (IServer) this);
+
+        } catch (Exception ex) {
+            System.out.println("Error en " + ex.getMessage());
+        }
     }
-  }
+
+    public ServerConfetti() throws RemoteException{
+        super();
+        clientes = new ArrayList<>();
+    }
     
     public static void main(String[] args) throws RemoteException {
-          (new ServerConfetti()).init();
+        (new ServerConfetti()).init();
     }
 
     @Override
@@ -64,5 +66,5 @@ public class ServerConfetti implements IServer {
     public void añadirPreguntas() throws RemoteException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
 }
