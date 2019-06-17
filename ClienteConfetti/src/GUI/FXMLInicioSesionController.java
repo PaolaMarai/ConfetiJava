2c508033ad005bcbe845f54389e26df1a23a4f63
ang0823 <angelsanchez934@gmail.com>
2019-06-17 15:32:50 -0500

    Se agrego metodo closeButtonOnAction

--- ClienteConfetti/src/GUI/FXMLInicioSesionController.java
+++ ClienteConfetti/src/GUI/FXMLInicioSesionController.java
@@ -32,192 +32,199 @@ import javafx.stage.Stage;
  */
 public class FXMLInicioSesionController implements Initializable {
 
-    @FXML
-    private TextField txtUser;
+  @FXML
+  private TextField txtUser;
 
-    @FXML
-    private PasswordField txtPassword;
+  @FXML
+  private PasswordField txtPassword;
 
-    @FXML
-    private Button btEntrar;
+  @FXML
+  private Button btEntrar;
 
-    @FXML
-    private Label lbRegistrate;
+  @FXML
+  private Label lbRegistrate;
 
-    @FXML
-    private Label lblError;
+  @FXML
+  private Label lblError;
 
-    public void loginUsuario() {
-        String error = "";
-        error = validarCampos();
-        if (!error.isEmpty()) {
-            lblError.setText(error);
+  public void loginUsuario() {
+    String error = "";
+    error = validarCampos();
+    if (!error.isEmpty()) {
+      lblError.setText(error);
+    } else {
+      if (autenticarCredenciales()) {
+        System.out.println("Usuario Autenticado");
+        if (Cliente.sesion.isAdmin() == true) {
+          System.out.println("es admin");
         } else {
-            if (autenticarCredenciales()) {
-                System.out.println("Usuario Autenticado");
-                if(Cliente.sesion.isAdmin() == true) {
-                    System.out.println("es admin");
-                } else {
-                    if(existEmision()) {
-                        if(isEmisionActive()) {
-                            System.out.println("pantalla emision");
-                            cargarPantallaEmision();
-                        } else {
-                            System.out.println("pantalla contador");
-                            cargarPantallaContadorEmision();
-                        }
-                    } else {
-                        System.out.println("pantalla contador");
-                        cargarPantallaContadorEmision();
-                    }
-                }
+          if (existEmision()) {
+            if (isEmisionActive()) {
+              System.out.println("pantalla emision");
+              cargarPantallaEmision();
             } else {
-                System.out.println("Credenciales incorrectas");
+              System.out.println("pantalla contador");
+              cargarPantallaContadorEmision();
             }
+          } else {
+            System.out.println("pantalla contador");
+            cargarPantallaContadorEmision();
+          }
         }
-    }
-
-    public String validarCampos() {
-        String errores = "";
-        if (txtUser.getText().isEmpty() || txtPassword.getText().isEmpty()) {
-            errores = errores + "Por favor rellene todos los campos\n";
-        }
-        return errores;
-    }
-
-    public boolean autenticarCredenciales() {
-        boolean autenticado = false;
-        Usuario usuarioLogin = null;
-        try {
-            usuarioLogin = Cliente.server.buscarUsuarioPorUsuarioSe(txtUser.getText());
-        } catch (RemoteException ex) {
-            Logger.getLogger(FXMLInicioSesionController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        if (usuarioLogin != null) {
-            boolean match = false;
-            String pass = txtPassword.getText();
-            String passwordMatch = "";
-            try {
-                passwordMatch = PasswordUtils.hashPassword(pass);
-            } catch (NoSuchAlgorithmException ex) {
-                Logger.getLogger(FXMLInicioSesionController.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            if (passwordMatch.compareTo(usuarioLogin.getClave()) == 0) {
-                Cliente.sesion.setUsuario(usuarioLogin.getUsuario());
-                Cliente.sesion.setPass(usuarioLogin.getClave());
-                Cliente.sesion.setAdmin(false);
-                autenticado = true;
-                
-            }
-        }
-
-        return autenticado;
-    }
-    
-    @FXML
-    public void cargarPantallaRegistro() {
-        Stage stage = new Stage();
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("../GUI/FXMLRegistrar.fxml"));
-        Parent root = null;
-        try {
-            root = loader.load();
-        } catch (IOException ex) {
-            Logger.getLogger(FXMLInicioSesionController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        Scene scene = new Scene(root);
-        
-        stage.setScene(scene);
-        stage.setResizable(false);
-        stage.show();
-        
-    }
-    
-    @FXML
-    public void cargarPantallaEmision() {
-      Stage stage = new Stage();
-      try {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("../GUI/FXMLEmision.fxml"));
-        Parent root = loader.load();
-        Scene scene = new Scene(root);
-
-        stage.setScene(scene);
-        stage.setResizable(false);
-        stage.show();
-      } catch (IOException ex) {
-        Logger.getLogger(FXMLInicioSesionController.class.getName()).log(Level.SEVERE, null, ex);
+      } else {
+        System.out.println("Credenciales incorrectas");
       }
     }
-    
-    @FXML
-    public void cargarPantallaContadorEmision() {
-      Stage stage = new Stage();
-      try {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("../GUI/FXMLCountDown.fxml"));
-        Parent root = loader.load();
-        Scene scene = new Scene(root);
+  }
 
-        stage.setScene(scene);
-        stage.setResizable(false);
-        stage.show();
-      } catch (IOException ex) {
+  public String validarCampos() {
+    String errores = "";
+    if (txtUser.getText().isEmpty() || txtPassword.getText().isEmpty()) {
+      errores = errores + "Por favor rellene todos los campos\n";
+    }
+    return errores;
+  }
+
+  public boolean autenticarCredenciales() {
+    boolean autenticado = false;
+    Usuario usuarioLogin = null;
+    try {
+      usuarioLogin = Cliente.server.buscarUsuarioPorUsuarioSe(txtUser.getText());
+    } catch (RemoteException ex) {
+      Logger.getLogger(FXMLInicioSesionController.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    if (usuarioLogin != null) {
+      boolean match = false;
+      String pass = txtPassword.getText();
+      String passwordMatch = "";
+      try {
+        passwordMatch = PasswordUtils.hashPassword(pass);
+      } catch (NoSuchAlgorithmException ex) {
         Logger.getLogger(FXMLInicioSesionController.class.getName()).log(Level.SEVERE, null, ex);
       }
-    }
-    
-    @FXML
-    public void cargarPantallaAdministrarEmision() {
-      Stage stage = new Stage();
-      try {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("../GUI/FXMLCrearEmision.fxml"));
-        Parent root = loader.load();
-        Scene scene = new Scene(root);
+      if (passwordMatch.compareTo(usuarioLogin.getClave()) == 0) {
+        Cliente.sesion.setUsuario(usuarioLogin.getUsuario());
+        Cliente.sesion.setPass(usuarioLogin.getClave());
+        Cliente.sesion.setAdmin(false);
+        autenticado = true;
 
-        stage.setScene(scene);
-        stage.setResizable(false);
-        stage.show();
-      } catch (IOException ex) {
-        Logger.getLogger(FXMLInicioSesionController.class.getName()).log(Level.SEVERE, null, ex);
       }
     }
-    
-    public void closeButtonAction() {
-        
-    }
-    
-    public boolean isEmisionActive() {
-        List<Emision> listaEmision = null;
-        try {
-            listaEmision = Cliente.server.buscarTodasEmision();
-        } catch (RemoteException ex) {
-            Logger.getLogger(FXMLInicioSesionController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        Emision emision = null;
-        if(!listaEmision.isEmpty()) {
-            for(Emision e : listaEmision) emision = e;
-            if(emision.getEnemision() == 1) {
-                return true;
-            }
-        }
-        return false;
-    }
-    
-    public boolean existEmision() {
-        List<Emision> listaEmision = null;
-        try {
-            listaEmision = Cliente.server.buscarTodasEmision();
-        } catch (RemoteException ex) {
-            Logger.getLogger(FXMLInicioSesionController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return !listaEmision.isEmpty();
-        
-    }
 
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
+    return autenticado;
+  }
 
+  @FXML
+  public void cargarPantallaRegistro() {
+    Stage stage = new Stage();
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("../GUI/FXMLRegistrar.fxml"));
+    Parent root = null;
+    try {
+      root = loader.load();
+    } catch (IOException ex) {
+      Logger.getLogger(FXMLInicioSesionController.class.getName()).log(Level.SEVERE, null, ex);
     }
+    Scene scene = new Scene(root);
+
+    stage.setScene(scene);
+    stage.setResizable(false);
+    stage.show();
+
+  }
+
+  @FXML
+  public void cargarPantallaEmision() {
+    Stage stage = new Stage();
+    try {
+      FXMLLoader loader = new FXMLLoader(getClass().getResource("../GUI/FXMLEmision.fxml"));
+      Parent root = loader.load();
+      Scene scene = new Scene(root);
+
+      stage.setScene(scene);
+      stage.setResizable(false);
+      stage.show();
+      closeButtonAction();
+    } catch (IOException ex) {
+      Logger.getLogger(FXMLInicioSesionController.class.getName()).log(Level.SEVERE, null, ex);
+    }
+  }
+
+  @FXML
+  public void cargarPantallaContadorEmision() {
+    Stage stage = new Stage();
+    try {
+      FXMLLoader loader = new FXMLLoader(getClass().getResource("../GUI/FXMLCountDown.fxml"));
+      Parent root = loader.load();
+      Scene scene = new Scene(root);
+
+      stage.setScene(scene);
+      stage.setResizable(false);
+      stage.show();
+      closeButtonAction();
+    } catch (IOException ex) {
+      Logger.getLogger(FXMLInicioSesionController.class.getName()).log(Level.SEVERE, null, ex);
+    }
+  }
+
+  @FXML
+  public void cargarPantallaAdministrarEmision() {
+    Stage stage = new Stage();
+    try {
+      FXMLLoader loader = new FXMLLoader(getClass().getResource("../GUI/FXMLCrearEmision.fxml"));
+      Parent root = loader.load();
+      Scene scene = new Scene(root);
+
+      stage.setScene(scene);
+      stage.setResizable(false);
+      stage.show();
+      closeButtonAction();
+    } catch (IOException ex) {
+      Logger.getLogger(FXMLInicioSesionController.class.getName()).log(Level.SEVERE, null, ex);
+    }
+  }
+
+  @FXML
+  private void closeButtonAction() {
+    Stage stage = (Stage) btEntrar.getScene().getWindow();
+    stage.close();
+  }
+
+  public boolean isEmisionActive() {
+    List<Emision> listaEmision = null;
+    try {
+      listaEmision = Cliente.server.buscarTodasEmision();
+    } catch (RemoteException ex) {
+      Logger.getLogger(FXMLInicioSesionController.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    Emision emision = null;
+    if (!listaEmision.isEmpty()) {
+      for (Emision e : listaEmision) {
+        emision = e;
+      }
+      if (emision.getEnemision() == 1) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public boolean existEmision() {
+    List<Emision> listaEmision = null;
+    try {
+      listaEmision = Cliente.server.buscarTodasEmision();
+    } catch (RemoteException ex) {
+      Logger.getLogger(FXMLInicioSesionController.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    return !listaEmision.isEmpty();
+
+  }
+
+  /**
+   * Initializes the controller class.
+   */
+  @Override
+  public void initialize(URL url, ResourceBundle rb) {
+
+  }
 
 }
